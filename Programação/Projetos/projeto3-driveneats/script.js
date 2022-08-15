b1f2c11e0bf77ba80996f6b1449bc0d5c3f840d0
Pedro Reis <phbarcelosreis66@gmail.com>
2022-08-14 23:49:39 -0300

    sixth commit

--- Programação/Projetos/projeto3-driveneats/script.js
+++ Programação/Projetos/projeto3-driveneats/script.js
@@ -2,38 +2,54 @@
 function selecionar(num) {
     const pintar = document.querySelector(".selecionado");
     const caixapedidos = document.querySelector(num);
+    const findbutton = document.querySelector(".botao"+num);
+    const buttonon = document.querySelector(".mostrar");
     
-    if(pintar){
-        pintar.classList.remove('selecionado')
+    if(pintar !== null){
+        pintar.classList.remove('selecionado');
     }
-    if(pintar === caixapedidos){
-        pintar.classList.toggle('selecionado')
+    if(buttonon !== null){
+        buttonon.classList.remove('mostrar');
     }
-    caixapedidos.classList.toggle("selecionado");
+    
+
+    caixapedidos.classList.add("selecionado");
+    findbutton.classList.add("mostrar");
 }
 
 function selecionar1(num) {
     const pintar1 = document.querySelector(".selecionado1");
     const caixapedidos = document.querySelector(num);
+    const findbutton = document.querySelector(".button"+num);
+    const buttonon = document.querySelector(".mostrar1");
     
-    if(pintar1){
-        pintar1.classList.remove('selecionado1')
+    if(pintar1 !== null){
+        pintar1.classList.remove('selecionado1');
     }
-    if(pintar1 === caixapedidos){
-        pintar1.classList.toggle('selecionado1')
+    if(buttonon !== null){
+        buttonon.classList.remove('mostrar1');
     }
-    caixapedidos.classList.toggle("selecionado1");
+    
+
+    caixapedidos.classList.add("selecionado1");
+    findbutton.classList.add("mostrar1");
 }
 
 function selecionar2(num) {
     const pintar2 = document.querySelector(".selecionado2");
     const caixapedidos = document.querySelector(num);
+    const findbutton = document.querySelector(".button"+num);
+    const buttonon = document.querySelector(".mostrar2");
     
-    if(pintar2){
-        pintar2.classList.remove('selecionado2')
+    if(pintar2 !== null){
+        pintar2.classList.remove('selecionado2');
     }
-    if(pintar2 === caixapedidos){
-        pintar2.classList.toggle('selecionado2')
+    if(buttonon !== null){
+        buttonon.classList.remove('mostrar2');
     }
-    caixapedidos.classList.toggle("selecionado2");
+    
+
+    caixapedidos.classList.add("selecionado2");
+    findbutton.classList.add("mostrar2");
 }
+
