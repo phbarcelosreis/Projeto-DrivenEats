f332e21b85ea5a0b3345a50e3bad2ec6252fd455
Pedro Reis <phbarcelosreis66@gmail.com>
2022-08-15 12:48:41 -0300

    seventh

--- Programação/Projetos/projeto3-driveneats/script.js
+++ Programação/Projetos/projeto3-driveneats/script.js
@@ -1,9 +1,15 @@
-let caixapedidos
-let caixapedidos1
-let caixapedidos2
-let textof
-let cordif
-let pratoselecionado
+let caixapedidos;
+let caixapedidos1;
+let caixapedidos2;
+let textof;
+let cordif;
+let pratoselecionado;
+let bebidaselecionada;
+let sobremesaselecionada;
+let preçoprato = 0;
+let preçobebida = 0;
+let preçosobremesa = 0;
+let soma = 0;
 
 
 function selecionar(num) {
@@ -11,6 +17,7 @@ function selecionar(num) {
     const caixapedidos = document.querySelector(num);
     const findbutton = document.querySelector(".botao"+num);
     const buttonon = document.querySelector(".mostrar");
+    
     
     if(pintar !== null){
         pintar.classList.remove('selecionado');
@@ -22,6 +29,11 @@ function selecionar(num) {
 
     caixapedidos.classList.add("selecionado");
     findbutton.classList.add("mostrar");
+    pratoselecionado = caixapedidos.getElementsByClassName('título-comida')[0].innerHTML;
+    preçoprato = caixapedidos.getElementsByClassName('preço-comida')[0].innerHTML;
+    preçoprato = preçoprato.replace("R$ ","")
+    preçoprato = preçoprato.replace(",", ".")
+    final();
 }
 
 function selecionar1(num) {
@@ -40,6 +52,11 @@ function selecionar1(num) {
 
     caixapedidos1.classList.add("selecionado1");
     findbutton.classList.add("mostrar1");
+    bebidaselecionada = caixapedidos1.getElementsByClassName('título-comida')[0].innerHTML;
+    preçobebida = caixapedidos1.getElementsByClassName('preço-comida')[0].innerHTML;
+    preçobebida = preçobebida.replace("R$ ","")
+    preçobebida = preçobebida.replace(",", ".")
+    final();
 }
 
 function selecionar2(num) {
@@ -55,20 +72,45 @@ function selecionar2(num) {
         buttonon.classList.remove('mostrar2');
     }
     
-    //pratoselecionado = pintar2
     caixapedidos2.classList.add("selecionado2");
     findbutton.classList.add("mostrar2");
+    sobremesaselecionada = caixapedidos2.getElementsByClassName('título-comida')[0].innerHTML;
+    preçosobremesa = caixapedidos2.getElementsByClassName('preço-comida')[0].innerHTML;
+    preçosobremesa = preçosobremesa.replace("R$ ","")
+    preçosobremesa = preçosobremesa.replace(",", ".")
+    final();
 }
 
-function final(num){
+function final(){
     const pintar = document.querySelector(".selecionado");
     const pintar1 = document.querySelector(".selecionado1");
     const pintar2 = document.querySelector(".selecionado2");
     cordif = document.querySelector('.texto-final');
+    textof = document.querySelector('.texto');
 
     if(pintar !== null && pintar1 !== null && pintar2 !== null){
-        cordif.classList.add('pedido-concluido')
+        cordif.classList.add('pedido-concluido');
+        textof.innerHTML = 'Fechar pedido';
+        soma = (Number(preçobebida) + Number(preçoprato) + Number(preçosobremesa)).toFixed(2)
     }
 }
+
+function finalpedido(){
+    
+        let text;
+
+            text =
+                "Olá, gostaria de fazer o pedido: " +
+                " - Prato: " + pratoselecionado +
+                " - Bebida: " + bebidaselecionada +
+                " - Sobremesa: " + sobremesaselecionada +
+                " Total: R$" + soma;
+
+    window.open("https://wa.me/5522981429161?text=" + text);
+}
+
+
+
+
 
 
